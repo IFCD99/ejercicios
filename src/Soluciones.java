27a8a610550202d8a49a5ce895ac531869c22f85
RafaelAI25 <rafael.docencia23@gmail.com>
2025-05-08 10:29:05 +0100

    Primer commit

---
+++ src/Soluciones.java
@@ -0,0 +1,22 @@
+
+public class Soluciones {
+    public static void main(String[] args) {
+
+        // Declarar variables
+        int edad = 25;
+        double altura = 1.75;
+        String nombre = "Ana";
+        System.out.println(nombre + " tiene  " + edad + " años"  );
+
+        // Suma de enteros
+        int num1 = 2;
+        int num2 = 3;
+        int resultado = num1 + num2;
+        System.out.println(resultado);
+
+        // Área Rectangulo
+
+
+
+        }
+}
